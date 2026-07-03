8838c4f0d9e9a0ea22709713032003f47c5652f8
Shim-HyoungJoon <gudwns15963@gmail.com>
2026-07-03 15:15:26 +0900

    포스트 쓰기, 불러오기, 삭제, 수정 API 완료

--- router/posts.mjs
+++ router/posts.mjs
@@ -1,15 +1,30 @@
 import express from "express"
+import { isAuth } from "../middleware/auth.mjs"
+import * as postController from "../controller/posts.mjs"
 
 const router = express.Router()
 
 // 전체 포스트 가져오기
+// http://127.0.0.1:8080/post (GET)
+// http://127.0.0.1:8080/post?userid=apple (GET)
+router.get("/", isAuth, postController.getPosts)
 
 // 글번호에 대한 포스트 가져오기
+// http://127.0.0.1:8080/:id (GET)
+router.get("/:id", isAuth, postController.getPost)
 
 // 포스트 쓰기
+// http://127.0.0.1:8080/post (POST)
+router.post("/", isAuth, postController.createPost)
 
 // 포스트 수정하기
+// http://127.0.0.1:8080/post/:id (PUT)
+router.put("/:id", isAuth, postController.updatePost)
 
 // 포스트 삭제하기
+// http://127.0.0.1:8080/post/:id (DELETE)
+router.delete("/:id", isAuth, postController.deletePost)
+
+
 
 export default router
